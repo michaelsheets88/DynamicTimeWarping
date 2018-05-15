ec1d4fc0b1ee85a04f9067a7f99fa170b7f65d70
michael <sheetsmj@msoe.edu>
2018-05-15 12:17:27 -0500

    WIP plotting wavefiles and results

--- main.py
+++ main.py
@@ -2,12 +2,14 @@ from scipy.spatial.distance import euclidean
 from waveforms import WaveformConverter as wc
 import os
 from fastdtw import fastdtw
+import matplotlib.pyplot as plt
+import pandas as pd
 
 
 def get_waveform(position='first'):
     looking_for_file = True
     while looking_for_file:
-        filepath = input("Enter the name of the {0} wave file(include extension)".format(position))
+        filepath = input("Enter the name of the {0} wave file\n".format(position))
         looking_for_file = not os.path.isfile("{0}.wav".format(filepath))
     return filepath
 
@@ -19,8 +21,23 @@ def main():
     first_2d = wc.get_2d_array_from_waveform(first_form)
     second_2d = wc.get_2d_array_from_waveform(second_form)
 
+    first_df = pd.DataFrame(first_2d)
+    first_plot = first_df.plot(title=first_form)
+    first_plot.set_xlabel("Time(Microseconds)")
+
+    second_df = pd.DataFrame(second_2d)
+    second_plot = second_df.plot(title=second_form)
+    second_plot.set_xlabel("Time(Microseconds)")
+
     distance, path = fastdtw(first_2d, second_2d, dist=euclidean)
-    print(distance)
+
+    path_x = [path[i][0] for i in range(0, len(path))]
+    path_y = [path[i][1] for i in range(0, len(path))]
+    path_df = pd.DataFrame(path_y, index=path_x)
+    path_plot = path_df.plot(title="Path results for best distance: {0}".format(distance))
+    path_plot.set_xlabel("Time(Microseconds)")
+
+    plt.show()
 
 
 main()
